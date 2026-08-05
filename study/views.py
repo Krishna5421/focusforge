ae009ad054f887fbf7bdd9b552d6881a6b150074
Krishna5421 <krishnayadav54216@gmail.com>
2026-08-05 09:45:00 +0530

    feat(study): add study dashboard and subject creation views

--- study/views.py
+++ study/views.py
@@ -4,11 +4,42 @@ from django.contrib import messages
 from .models import Subject, StudySession, StudyFileLog
 from .forms import SubjectForm, StudySessionForm
 
+
 @login_required
 def study_dashboard(request):
-    pass
+    subjects = Subject.objects.filter(user=request.user)
+    sessions = StudySession.objects.filter(user=request.user)[:10]
+    recent_files = StudyFileLog.objects.filter(user=request.user)[:10]
+
+    if request.method == 'POST':
+        form = StudySessionForm(request.POST, user=request.user)
+        if form.is_valid():
+            session = form.save(commit=False)
+            session.user = request.user
+            session.save()
+            messages.success(request, 'Study session logged.')
+            return redirect('study:study_dashboard')
+    else:
+        form = StudySessionForm(user=request.user)
+
+    return render(request, 'study/study_dashboard.html', {
+        'subjects': subjects,
+        'sessions': sessions,
+        'recent_files': recent_files,
+        'form': form,
+    })
+
 
 @login_required
 def subject_create(request):
-    pass
-
+    if request.method == 'POST':
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            subject.user = request.user
+            subject.save()
+            messages.success(request, 'Subject added.')
+            return redirect('study:study_dashboard')
+    else:
+        form = SubjectForm()
+    return render(request, 'study/subject_form.html', {'form': form})
