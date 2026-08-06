e8b6713d0a1b83caee66f2c78f7e4fc2f8325f35
Krishna5421 <krishnayadav54216@gmail.com>
2026-08-06 08:58:48 +0530

    feat(achievements): update achievement list view

--- achievements/views.py
+++ achievements/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import UserAchievement
+from .utils import check_all_achievements
 
-# Create your views here.
+
+@login_required
+def achievement_list(request):
+    check_all_achievements(request.user)
+    unlocked = UserAchievement.objects.filter(user=request.user)
+    return render(request, 'achievements/achievement_list.html', {'unlocked': unlocked})
