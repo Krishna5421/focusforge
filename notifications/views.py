d3063266c5c68e5501b95dc55829a3f3bd7037a2
Krishna5421 <krishnayadav54216@gmail.com>
2026-08-08 00:04:32 +0530

    feat(notifications): update notification views

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Notification
 
-# Create your views here.
+
+@login_required
+def notification_list(request):
+    notifications = Notification.objects.filter(user=request.user)
+    return render(request, 'notifications/notification_list.html', {'notifications': notifications})
+
+
+@login_required
+def notification_mark_read(request, pk):
+    notification = get_object_or_404(Notification, pk=pk, user=request.user)
+    notification.is_read = True
+    notification.save()
+    return redirect('notifications:notification_list')
