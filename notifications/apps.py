fe53c60148ad085a3e5d6ea6e810472bceb65668
Krishna5421 <krishnayadav54216@gmail.com>
2026-08-09 00:34:01 +0530

    feat(notifications): add scheduled reminder jobs via django-apscheduler

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+import sys
+import os
 
 
 class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
+
+    def ready(self):
+        if 'runserver' in sys.argv and os.environ.get('RUN_MAIN') == 'true':
+            from .scheduler import start_scheduler
+            start_scheduler()
