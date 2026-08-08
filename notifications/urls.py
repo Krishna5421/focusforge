e091044f69ff74a3f32271d862d0fb9bfcedaa27
Krishna5421 <krishnayadav54216@gmail.com>
2026-08-09 00:34:26 +0530

    feat(notifications): add notification list and mark-read views

---
+++ notifications/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'notifications'
+
+urlpatterns = [
+    path('', views.notification_list, name='notification_list'),
+    path('<int:pk>/read/', views.notification_mark_read, name='notification_mark_read'),
+]
